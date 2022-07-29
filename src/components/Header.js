9a83e4d22516683e89cb404ed5bc713d137d1211
gelso <gelso02@live.com>
2022-07-29 10:35:59 -0300

    feito ate req 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-export default class Header extends Component {
+class Header extends Component {
   render() {
+    const { info } = this.props;
     return (
-      <div>Header</div>
+      <div>
+        <span data-testid="email-field">{info}</span>
+        <span data-testid="total-field">0</span>
+        <span data-testid="header-currency-field">BRL</span>
+
+      </div>
     );
   }
 }
+const mapStateToProps = (state) => {
+  console.log(state);
+  return ({
+    info: state.user.email,
+
+  });
+};
+Header.propTypes = {
+  info: PropTypes.string.isRequired,
+
+};
+
+export default connect(mapStateToProps)(Header);
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,18 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INIT_STATE = {
+  email: '',
+};
+
+function user(state = INIT_STATE, action) {
+  switch (action.type) {
+  case 'USER':
+    return {
+      ...state,
+      email: action.value,
+    };
+  default:
+    return state;
+  }
+}
+
+export default user;
